38db58ca2e85d510a68a68d33472c9bf8f405364
Noah Pang <noahpang@cs.ucsb.edu>
2023-08-18 19:56:13 -0700

    added remove button to cards

--- main.js
+++ main.js
@@ -1,36 +1,94 @@
-const myLibrary = [];
+const myLibrary = {};
 
 function Book(title, author, numPages, haveRead) {
-    this.title = title;
-    this.author = author;
-    this.numPages = numPages;
-    this.haveRead = haveRead;
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  this.haveRead = haveRead;
 }
 
 function addBookToLibrary(title, author, numPages, haveRead) {
-    const newBook = new Book(title, author, numPages, haveRead);
-    myLibrary.push(newBook);
+  const newBook = new Book(title, author, numPages, haveRead);
+  if (author in myLibrary) {
+    const bookFound = myLibrary[author].find((book) => book.title === title);
+    if(bookFound !== undefined) {
+        return false;
+    }
+    myLibrary[author].push(newBook);
+  } else {
+    myLibrary[author] = [newBook];
+  }
+  return true;
 }
 
 const cardsContainer = document.querySelector(".cards");
 
-function displayBooks(startIndex) {
-    for(let i = startIndex; i < myLibrary.length; i++) {
-        const thisBook = myLibrary[i];
+function displayBooksBy(author, startIndex) {
+    for (let i = startIndex; i < myLibrary[author].length; i++) {
+        // create card
+        const thisBook = myLibrary[author][i];
         const bookCard = document.createElement("div");
         bookCard.classList.add("card");
-        const bookCardContent = document.createTextNode(`${thisBook.title} by ${thisBook.author}\n${thisBook.numPages} pages\n${thisBook.haveRead ? "read" : "not yet read"}`);
-        bookCard.appendChild(bookCardContent);
+  
+        // create text content
+        const bookTitle = document.createElement("span");
+        bookTitle.classList.add("book-title");
+        bookTitle.textContent = thisBook.title;
+        
+  
+        const bookAuthor = document.createElement("span");
+        bookAuthor.classList.add("book-author");
+        bookAuthor.textContent = thisBook.author;
+  
+        const bookPages = document.createElement("span");
+        bookPages.classList.add("book-num-pages");
+        bookPages.textContent = thisBook.numPages;
+  
+        const bookRead = document.createElement("span");
+        bookRead.classList.add("book-read");
+        bookRead.textContent = thisBook.haveRead;
+  
+        // create remove button
+        const removeBtn = document.createElement("button");
+        removeBtn.classList.add("remove-card");
+        removeBtn.textContent = "Remove";
+        removeBtn.addEventListener("click", removeCard);
+  
+        // add content to card
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(bookPages);
+        bookCard.appendChild(bookRead);
+        bookCard.appendChild(removeBtn);
+  
+        // add card to container
         cardsContainer.appendChild(bookCard);
-    }
+      }
+}
+
+function displayBooks(authorList, startIndex) {
+  for (let i = 0; i < authorList.length; i++) {
+    displayBooksBy(authorList[i], startIndex);
+  }
 }
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
-displayBooks(0);
+// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+displayBooks(Object.keys(myLibrary), 0);
+
+function removeCard(e) {
+  const card = this.parentElement;
+  const bookAuthor = card.querySelector(".book-author");
+  const bookTitle = card.querySelector(".book-title");
+  myLibrary[bookAuthor.innerText] = myLibrary[bookAuthor.innerText].filter((book) => {
+    return book.title !== bookTitle.innerText;
+  });
+
+  card.remove();
+}
 
 // Add book dialog
 
@@ -46,78 +104,96 @@ const dialogAuthor = document.getElementById("author");
 const dialogNumPages = document.getElementById("num-pages");
 const dialogRead = document.getElementById("read");
 
+const dialogAddBookError = document.querySelector(".book-add-error");
+const dialogAddBookErrorMessage = document.createTextNode(`Book already in library!`);
 const dialogTitleError = document.querySelector(".title-error");
 const dialogTitleErrorMessage = document.createTextNode(`Must have a title!`);
 const dialogAuthorError = document.querySelector(".author-error");
-const dialogAuthorErrorMessage = document.createTextNode(`Must have an author!`);
+const dialogAuthorErrorMessage =
+  document.createTextNode(`Must have an author!`);
 const dialogNumPagesError = document.querySelector(".num-pages-error");
-const dialogNumPagesErrorNoInput = document.createTextNode(`Must have number of pages!`);
-const dialogNumPagesErrorNotInt = document.createTextNode(`Number of pages must be an integer!`);
+const dialogNumPagesErrorNoInput = document.createTextNode(
+  `Must have number of pages!`
+);
+const dialogNumPagesErrorNotInt = document.createTextNode(
+  `Number of pages must be an integer!`
+);
 
 showDialogButton.addEventListener("click", () => {
-    addBookDialog.showModal();
-})
+  addBookDialog.showModal();
+});
 
 addBookDialog.addEventListener("close", (e) => {
-    addBookForm.reset();
-    
-    dialogTitleError.textContent = "";
-    dialogAuthorError.textContent = "";
-    dialogNumPagesError.textContent = "";
-  });
+  addBookForm.reset();
+
+  dialogTitleError.textContent = "";
+  dialogAuthorError.textContent = "";
+  dialogNumPagesError.textContent = "";
+  dialogAddBookError.textContent = "";
+});
 
 cancelBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    addBookDialog.close();
+  e.preventDefault();
+  addBookDialog.close();
 });
 
 function updateErrorMessagesHide() {
-    if(dialogTitle.value !== "") {
-        dialogTitleError.textContent = "";
-    }
-    if(dialogAuthor.value !== "") {
-        dialogAuthorError.textContent = "";
-    }
-   if(dialogNumPages.value !== "" && Number.isInteger(Number(dialogNumPages.value))) {
-        dialogNumPagesError.textContent = "";
-    }
+  if (dialogTitle.value !== "") {
+    dialogTitleError.textContent = "";
+  }
+  if (dialogAuthor.value !== "") {
+    dialogAuthorError.textContent = "";
+  }
+  if (
+    dialogNumPages.value !== "" &&
+    Number.isInteger(Number(dialogNumPages.value))
+  ) {
+    dialogNumPagesError.textContent = "";
+  }
 }
 
-function updateErrorMessagesShow() {
-    if(dialogTitle.value === "") {
-        dialogTitleError.appendChild(dialogTitleErrorMessage);
+function updateErrorMessagesShow(bookInLibrary) {
+    if(bookInLibrary) {
+        dialogAddBookError.appendChild(dialogAddBookErrorMessage);
     }
-    if(dialogAuthor.value === "") {
-        dialogAuthorError.appendChild(dialogAuthorErrorMessage);
-    } 
-    if(dialogNumPages.value === "") {
-        dialogNumPagesError.appendChild(dialogNumPagesErrorNoInput);
-    } else if(!Number.isInteger(Number(dialogNumPages.value))) {
-        dialogNumPagesError.appendChild(dialogNumPagesErrorNotInt);
-    }
+  if (dialogTitle.value === "") {
+    dialogTitleError.appendChild(dialogTitleErrorMessage);
+  }
+  if (dialogAuthor.value === "") {
+    dialogAuthorError.appendChild(dialogAuthorErrorMessage);
+  }
+  if (dialogNumPages.value === "") {
+    dialogNumPagesError.appendChild(dialogNumPagesErrorNoInput);
+  } else if (!Number.isInteger(Number(dialogNumPages.value))) {
+    dialogNumPagesError.appendChild(dialogNumPagesErrorNotInt);
+  }
 }
-
-// document.addEventListener("click", (e) => {
-    
-//     let clickInside = modal.contains(e.target);
-//     if(!clickInside) {
-//         // addBookDialog.close();
-//     }
-// });
 
 dialogTitle.addEventListener("change", updateErrorMessagesHide);
 dialogAuthor.addEventListener("change", updateErrorMessagesHide);
 dialogNumPages.addEventListener("change", updateErrorMessagesHide);
 
 confirmBtn.addEventListener("click", (event) => {
-    event.preventDefault(); // We don't want to submit this fake form
-    if(dialogTitle.value !== "" && dialogAuthor.value !== "" && dialogNumPages.value !== "" 
-        && Number.isInteger(Number(dialogNumPages.value))) {
-        addBookToLibrary(dialogTitle.value, dialogAuthor.value, Number(dialogNumPages.value), dialogRead.checked);
-        displayBooks(myLibrary.length-1);
+  event.preventDefault(); // We don't want to submit this fake form
+  if (
+    dialogTitle.value !== "" &&
+    dialogAuthor.value !== "" &&
+    dialogNumPages.value !== "" &&
+    Number.isInteger(Number(dialogNumPages.value))
+  ) {
+    const addBookSuccess = addBookToLibrary(
+      dialogTitle.value,
+      dialogAuthor.value,
+      Number(dialogNumPages.value),
+      dialogRead.checked
+    );
+    if(addBookSuccess) {
+        displayBooks([dialogAuthor.value], myLibrary[dialogAuthor.value].length - 1);
         addBookDialog.close();
     } else {
-        updateErrorMessagesShow();
+        updateErrorMessagesShow(true);
     }
-    
-  });
+  } else {
+    updateErrorMessagesShow(false);
+  }
+});
